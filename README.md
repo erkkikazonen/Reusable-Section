9560f004bcf8bad2f283dd11e5b63bdeefdd56f9
Eki <erkki.kazonen@gmail.com>
2025-05-27 13:03:56 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# Reusable section
+
+## Brief
+
+Develop a reusable section based on the provided design.
+
+Even though the design only has 6 cards, the amount should not be limited by code and we should be able to add as many cards as we want.
+
+The buttons on the left side should function as filters. Clicking one makes it active and only the cards with corresponding tag should remain visible. Clicking another button deactivates the previously selected one. Add short transition animations to all state changes (visibility, hover, etc.).
+
+Feel free to use modern CSS and JS features, we don't develop websites for IE users anymore. Avoid using pixels for sizing. You shouldn't worry about being pixel perfect with the design, as things should scale according to user's browser size and system settings. Stick to the file structure of this folder. Do not use frameworks, external libraries or AI for this task.
+
+## Pay attention to
+
+- Responsiveness
+- HTML semantics
+- Consistent and descriptive naming
+- Code structuring and commenting
+- Reusability of the components
+- Accessibility (bonus points)
---
+++ script.js
@@ -0,0 +1,73 @@
+(() => {
+  "use strict";
+
+  let currentFilter = null;
+
+  window.filterSelection = function (c) {
+    var x, i;
+    x = document.getElementsByClassName("filterDiv");
+    if (c == "all") c = "";
+
+    if (currentFilter === c) {
+      c = "";
+      currentFilter = null;
+    } else {
+      currentFilter = c;
+    }
+
+    for (i = 0; i < x.length; i++) {
+      removeClass(x[i], "show");
+      if(c === "" || x[i].className.indexOf(c) > -1) {
+        addClass(x[i], "show")
+      }
+    }
+  }  
+
+  function addClass(element, name) {
+    var i, arr1, arr2;
+    arr1 = element.className.split(" ");
+    arr2 = name.split(" ");
+
+    for (i = 0; i < arr2.length; i++) {
+      if (arr1.indexOf(arr2[i]) == -1) {
+        element.className += " " + arr2[i];
+      }
+    }
+  }
+
+  function removeClass(element, name) {
+    var i, arr1, arr2;
+    arr1 = element.className.split(" ");
+    arr2 = name.split(" ");
+
+    for (i = 0; i < arr2.length; i++) {
+      while (arr1.indexOf(arr2[i]) > -1) {
+        arr1.splice(arr1.indexOf(arr2[i]), 1);
+      }
+    }
+    element.className = arr1.join(" ");
+  }
+
+  var btnContainer = document.getElementsByClassName("button-group")[0];
+  var btns = btnContainer.getElementsByClassName("btn");
+
+  for (var i = 0; i < btns.length; i++) {
+    btns[i].addEventListener("click", function () {
+      var current = document.getElementsByClassName("active");
+      if (current.length > 0) {
+        current[0].className = current[0].className.replace(" active", "");
+      }
+
+      if (!this.classList.contains("active")) {
+        this.className += " active";
+      }
+    });
+  }
+
+  window.addEventListener("DOMContentLoaded", () => {
+    var x = document.getElementsByClassName("filterDiv");
+    for (var i = 0; i < x.length; i++) {
+      addClass(x[i], "show");
+    }
+  })
+})();
